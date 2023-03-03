c522e7446663e66817f9dc869a84988af1b65b9d
buyeah1109 <buyeah1109@gmail.com>
2023-03-03 17:40:04 +0800

    inital commit

--- app.py
+++ app.py
@@ -5,31 +5,62 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+FREE_CODE = os.getenv("FREECODE")
 
-
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+    # if request.method == "POST":
+    #     animal = request.form["animal"]
+    #     response = openai.Completion.create(
+    #         model="text-davinci-003",
+    #         prompt=generate_prompt(animal),
+    #         temperature=0.6,
+    #     )
+    #     return redirect(url_for("index", result=response.choices[0].text))
+
+    # result = request.args.get("result")
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == 'POST':
+        name = request.form["name"]
+        code = request.form["code"]
+
+        return redirect(url_for("prompt", code=code))
+
+@app.route("/prompt", methods=["POST", "GET"])
+def prompt():
+
+    if request.method == 'GET':
+        code = request.args.get("code")
+        if code != FREE_CODE:
+            return redirect(url_for("index"))
+
+        result = request.args.get("result")
+        return render_template("prompt-1.html", result=result)
+    
+    elif request.method == 'POST':
+        message = request.form["message"]
+        task = request.form["select"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": generate_prompt(message, task)},
+            ]
         )
-        return redirect(url_for("index", result=response.choices[0].text))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        return redirect(url_for("prompt", result=response['choices'][0]['message']['content'], code=FREE_CODE))
+    
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(paragraph, task):
+    if task == "Academic Rewrite":
+        instruction = "Rewrite the following paragraph in academic and formal way, keep the meaning same and correct any wrong usage of grammar or sentence structure: \"{}\"".format(paragraph)
+    
+    elif task == "Translation to Chinese":
+        instruction = "Translate the following paragraph to Chinese: \"{}\"".format(paragraph)
+    
+    elif task == "Translation to English":
+        instruction = "Translate the following paragraph to English: \"{}\"".format(paragraph)
+       
+    return instruction
